a5b87416a64ca815aed319feed446769b496df4f
Isaac Ramirez <risaac658@gmail.com>
2023-05-07 17:37:12 -0600

    Agrgado menu-mobile

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const deskmenu = document.querySelector('.desk-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggledeskmenu );
+menuHamIcon.addEventListener('click', toggleMobileMenu );
 
 function toggledeskmenu(){
 deskmenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
