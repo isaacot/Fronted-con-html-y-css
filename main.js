3542afb7169330c3c1d801b8be422469b26b5fe2
Isaac Ramirez <risaac658@gmail.com>
2023-05-09 20:04:41 -0600

    Agregue carrito de compras y solucione problemas de sobreposicionamiento

--- main.js
+++ main.js
@@ -2,14 +2,40 @@ const menuEmail = document.querySelector('.navbar-email');
 const deskmenu = document.querySelector('.desk-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggledeskmenu );
 menuHamIcon.addEventListener('click', toggleMobileMenu );
+menuCarritoIcon.addEventListener('click', toggleCarritoAside );
 
 function toggledeskmenu(){
+    const isAsideClosed =aside.classList.contains('inactive');
+      
+    if (!isAsideClosed){
+        aside.classList.add('inactive');
+    }
 deskmenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
-    mobileMenu.classList.toggle('inactive');
+    const isAsideClosed =aside.classList.contains('inactive');
+      
+if (!isAsideClosed){
+    aside.classList.add('inactive');
 }
+mobileMenu.classList.toggle('inactive');
+}
+function toggleCarritoAside(){
+    const isMobileMenuClosed =mobileMenu.classList.contains('inactive');
+    const isdeskmenuClosed =deskmenu.classList.contains('inactive');
+      
+    if (!isdeskmenuClosed){
+        deskmenu.classList.add('inactive');
+    }  
+
+if (!isMobileMenuClosed){
+    mobileMenu.classList.add('inactive');
+}
+ aside.classList.toggle('inactive');
+   }
