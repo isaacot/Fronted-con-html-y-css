fd92d6da9b0ffbc2ce6ddf26c543fdde00baa24b
Isaac Ramirez <risaac658@gmail.com>
2023-05-16 20:15:16 -0600

    Agregue clase 12 solo a html y css

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const deskmenu = document.querySelector('.desk-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggledeskmenu );
@@ -11,19 +11,19 @@ menuHamIcon.addEventListener('click', toggleMobileMenu );
 menuCarritoIcon.addEventListener('click', toggleCarritoAside );
 
 function toggledeskmenu(){
-    const isAsideClosed =aside.classList.contains('inactive');
+    const isAsideClosed =shoppingCartContainer.classList.contains('inactive');
       
     if (!isAsideClosed){
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
 deskmenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
-    const isAsideClosed =aside.classList.contains('inactive');
+    const isAsideClosed =shoppingCartContainer.classList.contains('inactive');
       
 if (!isAsideClosed){
-    aside.classList.add('inactive');
+    shoppingCartContainer.classList.add('inactive');
 }
 mobileMenu.classList.toggle('inactive');
 }
@@ -38,7 +38,7 @@ function toggleCarritoAside(){
 if (!isMobileMenuClosed){
     mobileMenu.classList.add('inactive');
 }
- aside.classList.toggle('inactive');
+ shoppingCartContainer.classList.toggle('inactive');
    }
 
 const productList = [];
